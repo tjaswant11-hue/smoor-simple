8f31b09239fc976d5f62d77aa268853f13d90545
tjaswant11-hue <tjaswant11@gmail.com>
2026-05-22 00:36:16 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:'40px',fontFamily:'Arial',background:'black',color:'white',minHeight:'100vh'}}>
+      <h1>SMOOR Experience Centre</h1>
+      <p>Luxury chocolate experience destination.</p>
+      <a href='https://wa.me/919962305191'>WhatsApp Concierge</a>
+    </div>
+  )
+}
